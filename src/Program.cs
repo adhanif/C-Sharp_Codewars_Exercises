d9b76d58ac8dd9bd9729b7054c799c3879ed1096
ad.hanif <ad.hanif@outlook.com>
2024-03-17 19:08:16 +0200

    Chalenge#1 solved

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+// Chalenge#1: Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+
+//solution:
+// public class Kata
+
+// {
+//     public static void Main(string[] args)
+//     {
+//         Shortcut("codewars");
+//     }
+//     public static string Shortcut(string input)
+//     {
+//         string newString = Regex.Replace(input, "[aeiouAEIOU]", "");
+//         return newString;
+//     }
+
+// }
